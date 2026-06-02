d9d5548c01526b1e65cb53d223b02d93cd9c5caa
spinzar <73431676+spinzar@users.noreply.github.com>
2026-06-02 22:23:42 +0900

    Initial release v1.0.0

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, globalShortcut, ipcMain, clipboard } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 920,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.on('close', (e) => { e.preventDefault(); mainWindow.hide(); });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  globalShortcut.register('Alt+Shift+P', () => {
+    if (mainWindow.isVisible()) mainWindow.hide();
+    else { mainWindow.show(); mainWindow.focus(); }
+  });
+});
+
+ipcMain.on('copy-text', (event, text) => clipboard.writeText(text));
+ipcMain.handle('paste-text', async () => clipboard.readText());
